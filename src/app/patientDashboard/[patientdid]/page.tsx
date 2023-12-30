a6d113425ecebad757e5c7070484dae5aead46c6
jaydeepdey03 <jaydeep.dey03@gmail.com>
2023-12-31 00:47:37 +0530

    dashboard needs change in responsiveness

--- src/app/patientDashboard/[patientdid]/page.tsx
+++ src/app/patientDashboard/[patientdid]/page.tsx
@@ -14,13 +14,13 @@ export default function PatientDashboard({
   params: { patientdid: string };
 }) {
   return (
-    <div className="relative h-screen w-screen overflow-x-hidden">
+    <div className="relative h-full w-screen">
       {/* Background circles */}
       <div className="absolute -top-[100px] -z-10 h-72 w-72 rounded-full bg-pink-400 blur-[500px]" />
       <div className="absolute -top-[100px] left-1/2 -z-10 h-72 w-72 -translate-x-1/2 transform rounded-full bg-blue-400 blur-[500px]" />
       <div className="absolute -top-[100px] right-0 -z-10 h-72 w-72 rounded-full bg-purple-400 blur-[500px]" />
       {/* Main content */}
-      <div className="flex h-fit w-full items-end justify-between py-10 sm:justify-start sm:p-10">
+      <div className="flex h-[500px] w-full items-end justify-between py-10 sm:h-fit sm:justify-start sm:p-10">
         <div className="flex flex-1 flex-col items-center gap-4 sm:flex-row">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
@@ -34,17 +34,17 @@ export default function PatientDashboard({
           </div>
         </div>
       </div>
-      <div className="grid h-screen grid-flow-row grid-cols-1 gap-4 overflow-x-hidden p-8 xl:grid-cols-3 xl:grid-rows-3">
-        <Card className="order-last col-start-1 col-end-2 row-span-full flex h-full w-full flex-col sm:px-5">
+      <div className="xl:n grid h-screen grid-flow-row grid-cols-1 gap-4 p-8 xl:grid-cols-3 xl:grid-rows-3">
+        <Card className="order-last col-start-1 col-end-2 row-span-full flex h-[600px] w-full flex-col xl:h-full">
           <CardHeader>
             <CardTitle>All Appointment</CardTitle>
           </CardHeader>
           <div className="card-scroll h-full w-full overflow-y-scroll">
-            <CardContent className="p-0 px-3">
-              <div className="space-y-2">
+            <CardContent className="overflow-hidden p-0">
+              <div className="space-y-2 px-7">
                 {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((item) => (
                   <div
-                    className="flex items-center rounded-xl pr-1 hover:bg-slate-100"
+                    className="flex items-center rounded-xl hover:bg-slate-100"
                     key={item}
                   >
                     <div className="flex items-center gap-0 truncate sm:w-[70%]">
@@ -90,7 +90,7 @@ export default function PatientDashboard({
             </CardContent>
           </div>
         </Card>
-        <div className="order-first grid h-full w-full grid-cols-1 gap-2 rounded-xl xl:col-span-full xl:col-start-2 xl:col-end-4 xl:row-start-1 xl:row-end-2 xl:grid-cols-3">
+        <div className="order-first grid h-[600px] w-full grid-cols-1 gap-2 rounded-xl xl:col-span-full xl:col-start-2 xl:col-end-4 xl:row-start-1 xl:row-end-2 xl:h-full xl:grid-cols-3">
           <div className="flex h-full w-full flex-col items-center justify-center space-y-2 rounded-xl border-[1px] border-slate-200 bg-white">
             <Ruler className="h-[50px] w-[50px] text-slate-700" />
             <p className="text-sm font-medium">Height</p>
@@ -107,7 +107,7 @@ export default function PatientDashboard({
             <p className="text-3xl font-semibold">B+</p>
           </div>
         </div>
-        <div className="order-2 h-full w-full overflow-hidden rounded-xl bg-pink-300 xl:col-start-2 xl:col-end-4 xl:row-start-2 xl:row-end-4">
+        <div className="h-[600px] w-full overflow-hidden rounded-xl xl:col-start-2 xl:col-end-4 xl:row-start-2 xl:row-end-4 xl:h-full">
           <Card className="h-full">
             <CardHeader>
               <CardTitle>My Appointment</CardTitle>
